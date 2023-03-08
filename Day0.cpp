23b0407c9016da410faaa53bd7162f629efee9bd
Shraddha-Pandey <91560433+Shraddha-Pandey@users.noreply.github.com>
2023-03-08 21:15:58 +0530

    Create Day0.cpp

---
+++ Day0.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    string inputstr;
+    getline(cin, inputstr);
+    cout<<"Hello, World."<<endl;
+    cout<<inputstr;
+    
+    return 0;
+}
